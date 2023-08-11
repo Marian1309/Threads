8ff08047c219cb396dc3c59bede918fc53f36fb1
Marian <pidchashymaryan@gmail.com>
2023-08-11 11:05:23 +0300

    fix: renamed files

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,5 @@
 export { default as GlobalLoader } from './global-loader';
-export { default as Pagination } from './pagination';
+export { default as Pagination } from './pagination_';
 export { default as ProfileHeader } from './profile-header';
 export { default as SearchBar } from './search-bar';
 export { default as ThreadsTab } from './threads-tab';
--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -3,4 +3,4 @@ export { default as UpdateProfile } from './update-profile';
 export { default as PostThread } from './post-thread';
 export { default as DeletePost } from './delete-thread';
 
-export { default as Comment } from './comment';
+export { default as Comment } from './comment_';
